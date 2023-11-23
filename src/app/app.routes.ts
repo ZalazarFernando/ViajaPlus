4a28015076729266c6cf3bf040e1ba8080d673ad
Fernando Zalazar <zalazarfernando686@outlook.es>
2023-11-22 21:38:26 -0300

    principal bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ChooseChairComponent } from './pages/choose-chair/choose-chair.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { DestinationComponent } from './pages/destination/destination.component';
+import { TravelFindComponent } from './pages/travel-find/travel-find.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'about-us',
+        component: AboutUsComponent
+    },
+    {
+        path: 'choose-chair',
+        component: ChooseChairComponent
+    },
+    {
+        path: 'contact-us',
+        component: ContactUsComponent
+    },
+    {
+        path: 'destination',
+        component: DestinationComponent
+    },
+    {
+        path: 'travel-find',
+        component: TravelFindComponent
+    }
+];
