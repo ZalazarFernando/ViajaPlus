6b761911225b13e351983ad69bb1d6f71d3efe92
Fernando Zalazar <zalazarfernando686@outlook.es>
2023-11-23 01:39:40 -0300

    choose chair floor two

--- src/app/pages/choose-chair/choose-chair.component.ts
+++ src/app/pages/choose-chair/choose-chair.component.ts
@@ -9,5 +9,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './choose-chair.component.css'
 })
 export class ChooseChairComponent {
-
+  auxNumThirteen = Array(13).fill(0);
+  auxNumOne = Array(1).fill(0);
+  auxNumTen = Array(10).fill(0);
 }
